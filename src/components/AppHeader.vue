017d5a3e8f0567f8e04145b2c7ca6640aed4ded9
MirkoCifelli <ciffo4@hotmail.it>
2024-01-24 15:00:21 +0100

    Milestone 1 input and button

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,8 +1,11 @@
 <script>
+import { store } from '../store';
+
+
 export default {
     data() {
         return {
-
+            store
         };
     },
     methods: {
@@ -13,9 +16,30 @@ export default {
 
 <template>
     <header>
-        HEADER
+        <div class="container-header">
+            <div class="row">
+                <div class="col">
+                    <input
+                        v-model="store.searchText"
+                        type="text"
+                        class="form-control"
+                        placeholder="Search character"
+                        aria-label="Search character">
+                </div>
+                <div class="col">
+                    <button type="submit" class="btn btn-primary mb-3">
+                                    Search
+                    </button>
+                </div>
+            </div>
+        </div>
     </header>
 </template>
 
 <style lang="scss" scoped>
+.container-header{
+    width: 100%;
+    height: 160px;
+    padding: 20px;
+}
 </style>
